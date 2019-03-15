31b4a175221594b1e02d264897dde1ab07dabb21
thegrep01 <den.belotserkovets@gmail.com>
2019-03-16 01:48:38 +0200

    Initial Commit

---
+++ Huffman-code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Huffman-code
+//
+//  Created by Денис Белоцерковец on 3/16/19.
+//  Copyright © 2019 Денис Белоцерковец. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
